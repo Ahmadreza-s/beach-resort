3f4d46851a3a70831f133f74a0cdb69e80b0bc74
Ahmadreza Salehvand <ahmadreza.salehvand@gmail.com>
2020-03-30 19:16:42 +0430

    change hero component from rooms

--- src/pages/Rooms/Rooms.js
+++ src/pages/Rooms/Rooms.js
@@ -3,10 +3,11 @@ import Hero from '../../components/Hero/Hero';
 import './Rooms.css';
 import {Link} from 'react-router-dom';
 import Banner from '../../components/Banner/Banner';
+import defaultBg from '../../assets/images/room-4.jpeg';
 
 const Rooms = () => {
     return (
-        <Hero hero='roomsHero'>
+        <Hero>
             <Banner title='Our rooms'>
                 <Link to='/' className='btn-primary'>
                     return home
