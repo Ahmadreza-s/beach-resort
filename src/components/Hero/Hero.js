aa66f07ea1267e346777695adfe8344d26b072af
Ahmadreza Salehvand <ahmadreza.salehvand@gmail.com>
2020-03-30 19:15:37 +0430

    change some logic of hero component

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './Hero.css';
+import defaultBackground from '../../assets/images/defaultBcg.jpeg';
 
-const hero = props => {
+const hero = ({image, children}) => {
+    const classes = ['hero'];
+    classes.push(image ? 'roomsHero' : 'defaultHero');
     return (
-        <header className={props.hero}>
-            {props.children}
+        <header className={classes.join(' ')} style={{
+            background: image ? `url('${image}') center/cover no-repeat` :
+                `url("${defaultBackground}") center/cover no-repeat`
+        }}>
+            {children}
         </header>
     );
 };
